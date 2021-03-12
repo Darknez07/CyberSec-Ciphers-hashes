b5245ae81dcae43a2955493fa2f6affb6ab6b2d4
Darknez07 <rishikakkar42@gmail.com>
2021-03-12 23:45:58 +0530

    Cmd test

--- cryptozen/ciphers/Ceaser_Cipher.py
+++ cryptozen/ciphers/Ceaser_Cipher.py
@@ -1,30 +1,29 @@
-s = " "
-letters = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+s = ""
+letters = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~"
 letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 # .find(a letter) will give the position of the letter in the
 # string making it easier to identify the decryption
 # and encryption
 # When u add more letters but then your code barely works
 
-def encrpyt():
+
+def encrypt():
     global s
-    your_str = input('Enter to encrpyt: ')
-    k = int(input('Enter the shift of letters: '))
+    your_str = input("Enter to encrpyt: ")
+    k = int(input("Enter the shift of letters: "))
     for i in your_str:
-        s += letters[(letters.find(i)+k) % len(letters)]
+        s += letters[(letters.find(i) + k) % len(letters)]
     return s
 
 
 def decrypt(text):
     for j in range(len(letters)):
-        s = " "
+        s = ""
         for i in text:
-            if i == letters.find(" "):
-                s += " "
-            else:
-                s += letters[(letters.find(i) - j) % len(letters)]
+            s += letters[(letters.find(i) - j) % len(letters)]
 
         print(s)
+
 
 def decryptsimple(text):
     s = ""
@@ -37,3 +36,5 @@ def decryptsimple(text):
             else:
                 s += i
         print(s.lower().capitalize())
+
+print(decrypt(encrypt()))
