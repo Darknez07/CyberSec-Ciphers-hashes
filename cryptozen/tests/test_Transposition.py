86b279a81c5ef4740fe116004c4bd01df2ed0ba3
Darknez07 <rishikakkar42@gmail.com>
2021-04-03 00:41:42 +0530

    Added new tests

--- cryptozen/tests/test_Transposition.py
+++ cryptozen/tests/test_Transposition.py
@@ -10,13 +10,40 @@ class Test(unittest.TestCase):
         self.assertIsInstance(ans, str)
         self.assertNotEqual(x, obj.key)
 
-    def test_encrypt_same_key(self):
+    def test_encrypt_mod_key(self):
         obj = Transpose(210)
         x = obj.key
         wo = "Hello boys welcome to encryption"
         val = obj.encrypt(wo)
         self.assertEqual(x % len(wo), obj.key)
         self.assertIsNotNone(obj.key)
+
+    def test_encrypt_equal_length(self):
+        obj = Transpose(25)
+        k = obj.key
+        val = obj.encrypt("Hello from the encryption")
+        nk = obj.key
+        self.assertNotEqual(k, nk)
+
+    def check_empty_decenc(self):
+        obj = Transpose(123)
+        with self.assertRaises(Exception):
+            obj.encrypt("")
+        with self.assertRaises(Exception):
+            obj.decrypt("")
+
+    def test_encrypt(self):
+        obj = Transpose(12)
+        orig = "Code this in most logical way"
+        val = obj.encrypt(orig)
+        val2 = obj.decrypt(val)
+        self.assertEqual(orig, val2)
+
+    def test_decrypt(self):
+        obj = Transpose(12)
+        orig = "Code with reason and logic"
+        val = obj.encrypt(orig)
+        self.assertIsInstance(val, str)
 
 if __name__ == "__main__":
     unittest.main()
