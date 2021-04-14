c09426b44c6fb72ee9fc47eb7286c0180404b9c5
Rishi Kakkar <rishikakkar42@gmail.com>
2021-04-15 00:19:20 +0530

    Update __main__.py

--- cryptozen/cryptozen/__main__.py
+++ cryptozen/cryptozen/__main__.py
@@ -4,24 +4,38 @@ from cryptozen.Ceaser_Cipher import Ceaser
 import sys
 import os
 
+done = False
 try:
     if sys.argv[1] == "--ceaser":
-        print(Ceaser().encrpyt())
+        print(Ceaser().encrypt())
+        done = True
 except:
     print("Usage 'python -m cryptozen --ceaser'")
+
+if done:
+    sys.exit()
+
 # try:
 #     if sys.argv[1] == "--file":
 #         if os.path.exists(sys.argv[2]):
 #             print(Files.Files().use_files(filename=sys.argv[2]))
 #         else:
 #             print("Filename wrong")
+#         done = True
 # except:
 #     print("Usage 'python -m cryptozen --file filename.txt'")
+
+# if done:
+#     sys.exit()
 
 try:
     if sys.argv[1] == "--transpose" and int(sys.argv[2]):
         print(Transpose(key=int(sys.argv[2])).encrypt())
     else:
         print("Usage 'python -m cryptozen --transpose 12(any number)'")
+    done = True
 except:
     print("Usage 'python -m cryptozen --transpose 12(any number)'")
+
+if done:
+    sys.exit()
