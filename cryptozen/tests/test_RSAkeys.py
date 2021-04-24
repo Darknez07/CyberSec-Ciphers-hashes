b91094033eca71a7669e3e9877aba1b557afd8fe
Darknez07 <rishikakkar42@gmail.com>
2021-04-25 02:05:40 +0530

    Do not break code

--- cryptozen/tests/test_RSAkeys.py
+++ cryptozen/tests/test_RSAkeys.py
@@ -1,5 +1,5 @@
 import unittest
-from cryptozen.RSAkeys import low_primal_test
+from cryptozen.RSAkeys import low_primal_test, get_random_num
 import sympy as smp
 class Test(unittest.TestCase):
     @unittest.skip("This might fail because it is low prime test")
@@ -7,6 +7,11 @@ class Test(unittest.TestCase):
         ans = low_primal_test(12)
         self.assertEqual(len(bin(ans)[2:]), 12)
         self.assertTrue(smp.isprime(ans))
+
+    def test_for_random_num(self):
+        ans = get_random_num(12)
+        self.assertEqual(len(bin(ans)[2:]), 12)
+        self.assertTrue(2**11 <= ans <= 2**12)
 
 if __name__ == '__main__':
     unittest.main()
