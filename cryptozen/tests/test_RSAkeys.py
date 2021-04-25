88a672dd2ab51188916a2bd5f624da49e617b516
Darknez07 <rishikakkar42@gmail.com>
2021-04-26 00:49:15 +0530

    Added new test cases

--- cryptozen/tests/test_RSAkeys.py
+++ cryptozen/tests/test_RSAkeys.py
@@ -1,6 +1,7 @@
 import unittest
-from cryptozen.RSAkeys import low_primal_test, get_random_num
+from cryptozen.RSAkeys import low_primal_test, get_random_num, high_primal_test
 import sympy as smp
+import random
 class Test(unittest.TestCase):
     @unittest.skip("This might fail because it is low prime test")
     def test_low_prime(self):
@@ -12,6 +13,20 @@ class Test(unittest.TestCase):
         ans = get_random_num(12)
         self.assertEqual(len(bin(ans)[2:]), 12)
         self.assertTrue(2**11 <= ans <= 2**12)
+
+    def test_high_prime(self):
+        i = 0
+        done = set()
+        while i!=10:
+            s = smp.randprime(2**31, 2**32)
+            ans = high_primal_test(s)
+            if ans == s:
+                done.add(s)
+                i+=1
+            else:
+                self.assertEqual(1, 0)
+        for x in done:
+            self.assertTrue(smp.isprime(x))
 
 if __name__ == '__main__':
     unittest.main()
