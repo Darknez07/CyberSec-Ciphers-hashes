9d8910f170940beb3931ad6025363500d5ac31b8
Darknez07 <rishikakkar42@gmail.com>
2021-04-26 01:05:16 +0530

    More tests added

--- cryptozen/tests/test_RSAkeys.py
+++ cryptozen/tests/test_RSAkeys.py
@@ -18,7 +18,7 @@ class Test(unittest.TestCase):
         i = 0
         done = set()
         while i!=10:
-            s = smp.randprime(2**31, 2**32)
+            s = smp.randprime(2**200, 2**210)
             ans = high_primal_test(s)
             if ans == s:
                 done.add(s)
@@ -27,6 +27,16 @@ class Test(unittest.TestCase):
                 self.assertEqual(1, 0)
         for x in done:
             self.assertTrue(smp.isprime(x))
+
+    def test_low_primal(self):
+        while True:
+            s = smp.randprime(2**11, 2**12)
+            ans = low_primal_test(12)
+            if smp.isprime(ans):
+                break
+        self.assertEqual(len(bin(ans)[2:]), 12)
+        # self.assertTrue(smp.isprime(ans))
+        self.assertEqual(high_primal_test(ans), ans)
 
 if __name__ == '__main__':
     unittest.main()
