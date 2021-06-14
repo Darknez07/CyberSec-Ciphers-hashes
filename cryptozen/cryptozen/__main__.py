724e49f43d1bb48e513190a20ca5dc9cbf433bcf
Darknez07 <rishikakkar42@gmail.com>
2021-06-14 17:07:31 +0530

    Edited main file

--- cryptozen/cryptozen/__main__.py
+++ cryptozen/cryptozen/__main__.py
@@ -1,43 +1,57 @@
 from cryptozen.Transposition import Transpose
-from cryptozen.Ceaser_Cipher import Ceaser
+from cryptozen.Ceaser_Cipher import Ceaser,letters
 from cryptozen import Files
-import sys
 import os
 import argparse
 #shift to argparse
 
-done = False
-try:
-    if sys.argv[1] == "--ceaser":
-        print(Ceaser().encrypt())
-        done = True
-except:
-    print("Usage 'python -m cryptozen --ceaser'")
+parser = argparse.ArgumentParser()
+parser.add_argument('--ceaser',action='store',type=int,help='Ceaser cipher with specified number')
+parser.add_argument('--transpose',action='store',type=int)
+args = parser.parse_args()
+print(args.ceaser)
+print(args.transpose)
 
-if done:
-    sys.exit()
+if args.ceaser:
+    cipher = Ceaser(args.ceaser)
+    print(cipher.encrypt())
+elif args.transpose:
+    cipher = Transpose(args.transpose)
+    print(cipher.encrypt())
 
-try:
-    if sys.argv[1] == "--file":
-        if os.path.exists(sys.argv[2]):
-            print(Files.Files().use_files(filename=sys.argv[2]))
-        else:
-            print("Filename wrong")
-        done = True
-except:
-    print("Usage 'python -m cryptozen --file filename.txt'")
+# done = False
 
-if done:
-    sys.exit()
+# try:
+#     if sys.argv[1] == "--ceaser":
+#         print(Ceaser().encrypt())
+#         done = True
+# except:
+#     print("Usage 'python -m cryptozen --ceaser'")
 
-try:
-    if sys.argv[1] == "--transpose" and int(sys.argv[2]):
-        print(Transpose(key=int(sys.argv[2])).encrypt())
-    else:
-        print("Usage 'python -m cryptozen --transpose 12(any number)'")
-    done = True
-except:
-    print("Usage 'python -m cryptozen --transpose 12(any number)'")
+# if done:
+#     sys.exit()
 
-if done:
-    sys.exit()
+# try:
+#     if sys.argv[1] == "--file":
+#         if os.path.exists(sys.argv[2]):
+#             print(Files.Files().use_files(filename=sys.argv[2]))
+#         else:
+#             print("Filename wrong")
+#         done = True
+# except:
+#     print("Usage 'python -m cryptozen --file filename.txt'")
+
+# if done:
+#     sys.exit()
+
+# try:
+#     if sys.argv[1] == "--transpose" and int(sys.argv[2]):
+#         print(Transpose(key=int(sys.argv[2])).encrypt())
+#     else:
+#         print("Usage 'python -m cryptozen --transpose 12(any number)'")
+#     done = True
+# except:
+#     print("Usage 'python -m cryptozen --transpose 12(any number)'")
+
+# if done:
+#     sys.exit()
