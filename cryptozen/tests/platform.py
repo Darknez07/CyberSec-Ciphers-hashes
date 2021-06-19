852b3076c59f75358909d039ae730f3c786753e2
Darknez07 <rishikakkar42@gmail.com>
2021-06-20 02:01:31 +0530

    Added important comment

--- cryptozen/tests/platform.py
+++ cryptozen/tests/platform.py
@@ -1,5 +1,6 @@
 import sys
-
+# This file is for adding the filepath to python
+# settings
 plt = sys.platform
 if plt.startswith("linux"):
     sys.path.insert(0, "/".join(sys.path[0].split("/")[:-1]) + "/cryptozen")
