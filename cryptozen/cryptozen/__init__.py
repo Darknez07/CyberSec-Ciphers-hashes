5a7251d4bf6985274cb1a1bc59ab0a47d493880e
Darknez07 <rishikakkar42@gmail.com>
2022-09-18 18:15:31 +0530

    Entry code changes

--- cryptozen/cryptozen/__init__.py
+++ cryptozen/cryptozen/__init__.py
@@ -1,3 +1,4 @@
 __version__ = "0.0.1"
 __author__ = "darknez07"
 __fun__ = "Fun project"
+# __name__ = "main"
--- cryptozen/cryptozen/__main__.py
+++ cryptozen/cryptozen/__main__.py
@@ -3,14 +3,17 @@ from cryptozen.Ceaser_Cipher import Ceaser,letters
 from cryptozen import Files
 import os
 import argparse
+from cryptozen.distribution import Plots
 # shift to argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--ceaser',action='store',type=int,help='Ceaser cipher with specified number')
 parser.add_argument('--transpose',action='store',type=int)
+parser.add_argument('--plot',action="store",type=str,help='Enter a message to see distributin',metavar='P')
 args = parser.parse_args()
 print(args.ceaser)
 print(args.transpose)
+print(args.plot)
 
 if args.ceaser:
     cipher = Ceaser(args.ceaser)
@@ -18,6 +21,13 @@ if args.ceaser:
 elif args.transpose:
     cipher = Transpose(args.transpose)
     print(cipher.encrypt())
+elif args.plot:
+    cipher = Plots()
+    if ".txt" in args.plot:
+        print(cipher.analyze_with_file(args.plot))
+    else:
+        cipher.draw(args.plot,show=True,types="density")
+        cipher.analysis(show=True)
 
 # done = False
 
